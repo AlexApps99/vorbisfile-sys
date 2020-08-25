fc87c426cd0661221731d5dc5c933a11dfbbc5e9
AlexApps99 <alex.apps99@gmail.com>
2020-08-25 12:28:55 +1200

    Version bump

--- libvorbisfile/build.rs
+++ libvorbisfile/build.rs
@@ -1,5 +1,5 @@
 extern crate pkg_config;
-extern crate gcc;
+extern crate cc;
 
 use std::path::Path;
 
@@ -15,7 +15,7 @@ fn main() {
     let inc2 = std::env::var("DEP_VORBIS_SRC").unwrap();
     let inc3 = std::env::var("DEP_OGG_INCLUDE").unwrap();
 
-    gcc::Config::new()
+    cc::Build::new()
                 .file("libvorbisfile/vorbisfile.c")
                 .define("_USRDLL", None)
                 .define("LIBVORBIS_EXPORTS", None)
